de8fbd303ebee820cf5043bc00024a760d194c02
Maxwell Farrell <maxwellrichardfarrell@gmail.com>
2015-04-27 11:40:06 +0100

    add monokai and remove themes

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -8,6 +8,8 @@
              '("marmalade" . "http://marmalade-repo.org/packages/") t)
 (add-to-list 'package-archives
              '("tromey" . "http://tromey.com/elpa/") t)
+(add-to-list 'package-archives 
+             '("melpa" . "http://melpa.milkbox.net/packages/") t)
 
 ;; (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
 ;;                          ("marmalade" . "http://marmalade-repo.org/packages/")
@@ -121,7 +123,7 @@
 
 ;; Hard-to-categorize customizations
 (load "misc.el")
-
+x
 ;; For editing lisps
 (load "elisp-editing.el")
 
@@ -129,9 +131,19 @@
 (load "setup-clojure.el")
 (load "setup-js.el")
 
+
+;;some git stuff
+(global-git-gutter-mode)
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(coffee-tab-width 2))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(linum ((t (:foreground "#FCF7FC")))))
